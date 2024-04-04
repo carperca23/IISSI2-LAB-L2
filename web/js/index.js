22789c7c8f148124aa45c1cb275281aad0b1647a
carperca23 <carlosperezcantalapiedra23@gmail.com>
2024-04-04 12:27:16 +0200

    L6

--- web/js/index.js
+++ web/js/index.js
@@ -1,48 +1,33 @@
 "use strict";
-import { galleryRenderer } from '/js/renderers/gallery.js';
-function main() {
-    let container = document.getElementById("gallery");
-    let photos = [
-        {
-            title: "Samoyed",
-            description: "A very good boy.",
-            userId: 1,
-            url: "https://i.ibb.co/tY1Jcnc/wlZCfCv.jpg",
-            date: "15/08/2020",
-        },
-        {
-            title: "ETSII",
-            description: "E.T.S. Ing. Informatica, Universidad de Sevilla",
-            userId: 2,
-            url: "https://cdn.pixabay.com/photo/2023/04/13/09/56/alps-7922246_1280.jpg",
-            date: "01/01/2021",
-        },
-        {
-            title: "Seville",
-            description: "The beautiful city of Seville",
-            userId: 3,
-            url: "https://urbansevilla.es/wp-content/uploads/2019/03/urban-sevilla-foto-ciudad.jpg",
-            date: "03/02/2019",
-        },
-        {
-            title: "Abstract art",
-            description: "Clipart",
-            userId: 4,
-            url: "https://cdn.pixabay.com/photo/2023/04/11/13/27/bird-7917250_1280.jpg",
-            date: "14/08/2019",
-        },
-    ];
-    
-    let gallery = galleryRenderer.asCardGallery(photos);
-    container.appendChild(gallery);
+import { photosAPI_auto } from "/js/api/_photos.js";
+import { galleryRenderer } from "/js/renderers/gallery.js";
+import { messageRenderer } from "/js/renderers/messages.js";
+import { photoswithusersAPI_auto } from "/js/api/_photoswithusers.js";
 
+async function main() {
+
+    loadAllPhotos();
+}
+async function loadAllPhotos() {
+    let galleryContainer = document.querySelector("div.container");
+    try {
+        let photos = await photoswithusersAPI_auto.getAll();
+        let cardGallery = galleryRenderer.asCardGallery(photos);
+        galleryContainer.appendChild(cardGallery);
+        change_border();
+    } catch (err) {
+        messageRenderer.showErrorMessage("Error while loading photos",
+            err);
+    }
+}
+
+async function change_border(){
     let cards = document.querySelectorAll("div.card");
-
+    
     for (let card of cards) {
         card.onmouseenter = handleMouseEnter;
         card.onmouseleave = handleMouseLeave;
     }
-
 }
 
 function handleMouseEnter(event) {
@@ -55,5 +40,5 @@ function handleMouseLeave(event) {
     card.style.border = "none";
 }
 
-
 document.addEventListener("DOMContentLoaded", main);
+
--- web/js/photoDetail.js
+++ web/js/photoDetail.js
@@ -1,16 +1,27 @@
 "use strict";
 import { photoRenderer } from "/js/renderers/photos.js";
-function main() {
-let photoContainer = document.querySelector("#photo-details-column")
-;
-let photo = {
-title: "Samoyed",
-description: "A very good boy.",
-userId: 7,
-url: "https://i.ibb.co/tY1Jcnc/wlZCfCv.jpg",
-date: "12/01/1996",
-};
-let photoDetails = photoRenderer.asDetails(photo);
-photoContainer.appendChild(photoDetails);
+import { photosAPI_auto } from "/js/api/_photos.js";
+import { messageRenderer } from "/js/renderers/messages.js";
+// Get the ID of the photo to load from the URL params
+let urlParams = new URLSearchParams(window.location.search);
+let photoId = urlParams.get("photoId");
+async function main() {
+    // Check that we have an ID before doing anything else
+    if (photoId === null) {
+        messageRenderer.showErrorMessage("Please, provide a photoId");
+        return;
+    }
+    loadPhotoDetails();
+}
+async function loadPhotoDetails() {
+    let photoContainer = document.querySelector("#photo-details-column")
+        ;
+    try {
+        let photo = await photosAPI_auto.getById(photoId)
+        let photoDetails = photoRenderer.asDetails(photo);
+        photoContainer.appendChild(photoDetails);
+    } catch (err) {
+        messageRenderer.showErrorMessage("Error loading photo", err);
+    }
 }
 document.addEventListener("DOMContentLoaded", main);
