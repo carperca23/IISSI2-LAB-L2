6bc1baa86bcdea14308f0f4091efdd2e961bcd0e
carperca23 <carlosperezcantalapiedra23@gmail.com>
2024-04-24 21:24:35 +0200

    Laboratorio 7 terminado

--- web/js/edit_photo.js
+++ web/js/edit_photo.js
@@ -1,9 +1,17 @@
 "use strict";
 
-import { photosAPI } from "/js/api/photos.js";
 import { messageRenderer } from "/js/renderers/messages.js";
+import { photosAPI_auto } from "/js/api/_photos.js";
+
+let urlParams = new URLSearchParams(window.location.search);
+let photoId = urlParams.get("photoId");
+let currentPhoto = null;
 
 async function main() {
+    if (photoId !== null) {
+        loadCurrentPhoto();
+        }
+
     let registerForm = document.getElementById("form-photo-upload");
     registerForm.onsubmit = handleSubmitPhoto;
 }
@@ -12,16 +20,47 @@ async function handleSubmitPhoto(event) {
     event.preventDefault();
     let form = event.target;
     let formData = new FormData(form);
-    // Add the current user ID
-    formData.append("userId", 1);
-    try {
-    let resp = await photosAPI_auto.create(formData);
-    let newId = resp.lastId;
-    window.location.href = `photo_detail.html?photoId=${newId}`;
-    } catch (err) {
-    messageRenderer.showErrorMessage(err.response.data.message);
+    if (currentPhoto === null) { // Creating a new photo
+        // Add the current user ID
+        formData.append("userId", 1);
+        try {
+            let resp = await photosAPI_auto.create(formData);
+            let newId = resp.lastId;
+            window.location.href = `photo_detail.html?photoId=${newId}`;
+        } catch (err) {
+            messageRenderer.showErrorAsAlert(err.response.data.message);
+        }
+    } else { // Updating an existing photo
+        formData.append("userId", currentPhoto.userId);
+        formData.append("date", currentPhoto.date);
+        try {
+            await photosAPI_auto.update(formData, photoId);
+            window.location.href = `photo_detail.html?photoId=${photoId}
+`;
+        } catch (err) {
+            messageRenderer.showErrorAsAlert(err.response.data.message);
+        }
     }
 }
-    
+
+async function loadCurrentPhoto() {
+
+    let urlInput = document.getElementById("input-url");
+    let titleInput = document.getElementById("input-title");
+    let descriptionInput = document.getElementById("input-description");
+    let visibilityInput = document.getElementById("input-visibility");
+
+    document.title = "Editing a photo";
+    try {
+        currentPhoto = await photosAPI_auto.getById(photoId);
+        urlInput.value = currentPhoto.url;
+        titleInput.value = currentPhoto.title;
+        descriptionInput.value = currentPhoto.description;
+        visibilityInput.value = currentPhoto.visibility;
+    } catch (err) {
+        messageRenderer.showErrorMessage(err.response.data.message);
+    }
+
+}
 
 document.addEventListener("DOMContentLoaded", main);
--- web/js/photoDetail.js
+++ web/js/photoDetail.js
@@ -5,14 +5,41 @@ import { messageRenderer } from "/js/renderers/messages.js";
 // Get the ID of the photo to load from the URL params
 let urlParams = new URLSearchParams(window.location.search);
 let photoId = urlParams.get("photoId");
+
 async function main() {
     // Check that we have an ID before doing anything else
     if (photoId === null) {
         messageRenderer.showErrorMessage("Please, provide a photoId");
         return;
-    }
+    } 
     loadPhotoDetails();
+
+    let deleteBtn =  document.querySelector("#button-delete");
+    deleteBtn.onclick =  handleDelete;
+
+    let editBtn = document.querySelector("#button-edit");
+    editBtn.onclick = handleEdit;
 }
+
+
+
+function handleEdit(event) {
+    window.location.href = "edit_photo.html?photoId=" + photoId;
+}
+
+async function handleDelete(event) {
+    let answer = confirm("Do you really want to delete this photo?");
+
+    if (answer) {
+        try {
+            await photosAPI_auto.delete(photoId);
+            window.location = "/index.html";
+        } catch (err) {
+            messageRenderer.showErrorMessage(err.response.data.message);
+        }
+    }
+}
+
 async function loadPhotoDetails() {
     let photoContainer = document.querySelector("#photo-details-column")
         ;
